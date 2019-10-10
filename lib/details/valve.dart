ac29733d45969964b100a4ab1611a200c8bdb047
Julian Winter <julian.winter@dm.de>
2019-10-10 11:05:08 +0200

    Removed unused stateful widget

--- lib/details/valve.dart
+++ lib/details/valve.dart
@@ -4,29 +4,24 @@ import 'package:garden_madam/butler-feed.dart';
 
 import '../model.dart';
 
-class ValvesListItem extends StatefulWidget {
-  ButlerController butlerController;
-  Pin pin;
+class ValvesListItem extends StatelessWidget {
+  ButlerController _butlerController;
+  Pin _pin;
 
-  ValvesListItem(this.butlerController, this.pin);
+  ValvesListItem(this._butlerController, this._pin);
 
-  @override
-  _ValvesListItemState createState() => _ValvesListItemState();
-}
-
-class _ValvesListItemState extends State<ValvesListItem> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {print("tap");},
       leading: new GestureDetector(
-        onTap: () => widget.pin.toggle(widget.butlerController),
+        onTap: () => _pin.toggle(_butlerController),
         child: Container(
           decoration: BoxDecoration(
             border: Border.all(
                 width: 5,
                 color:
-                    widget.pin.status == Status.ON ? Colors.blue : Colors.grey),
+                    _pin.status == Status.ON ? Colors.blue : Colors.grey),
             borderRadius: BorderRadius.circular(50),
           ),
           margin: const EdgeInsets.fromLTRB(4, 4, 50, 4),
@@ -36,19 +31,19 @@ class _ValvesListItemState extends State<ValvesListItem> {
           alignment: Alignment.center,
           child: SvgPicture.asset(
               'images/' +
-                  (widget.pin.imageName != null
-                      ? widget.pin.imageName
+                  (_pin.imageName != null
+                      ? _pin.imageName
                       : 'valve_1.svg'),
               semanticsLabel: 'Butler default image'),
         ),
       ),
       title: Text(
-        widget.pin.name != null
-            ? widget.pin.name
-            : widget.pin.valvePinNumber.toString(),
+        _pin.name != null
+            ? _pin.name
+            : _pin.valvePinNumber.toString(),
         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
-      trailing: _getScheduleIcon(widget.pin),
+      trailing: _getScheduleIcon(_pin),
 
     );
   }
